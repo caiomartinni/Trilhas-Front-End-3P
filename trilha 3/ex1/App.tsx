b3c01c5f083c9639459d504261d21595cb2e4afd
caiomartinni <caio.martini@grupointegrado.br>
2025-04-16 19:36:22 -0300

    trilha 3 ex1

---
+++ trilha 3/ex1/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
